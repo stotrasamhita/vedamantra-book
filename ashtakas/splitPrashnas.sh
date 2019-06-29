336e04f386f9f61709da40392ec4106e96b3ce46
Karthik Raman <karthik.raman@gmail.com>
2019-06-29 20:54:42 +0530

    fixes/updates

--- ashtakas/splitPrashnas.sh
+++ ashtakas/splitPrashnas.sh
@@ -6,5 +6,6 @@ awk 'BEGIN{c=0; p=0}
 /chapt/,/%%% END ASHTAKAM/{print > "TaittiriyaBrahmanam-Ashtakam-" c ".tex"}
 /sect/,/prashnaend/{print > "TaittiriyaBrahmanam-" c "-" p ".tex"}' TaittiriyaBrahmanam-Ashtakams.tex
 cd .old/
-for x in *.tex; do diff $x ../$x -qs && mv -v $x ../; done
+for x in *.tex; do diff $x ../$x -q && mv $x ../; done
+#for x in *.tex; do diff $x ../$x -qs && mv -v $x ../; done
 cd ..
