3bb015fcf9c2c2611160fa777066138e7780202c
Karthik Raman <karthikraman@users.noreply.github.com>
2020-01-11 11:58:39 +0530

    careful checking for non-mahAprANa before स

--- cleanText.py
+++ cleanText.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
 import sys
+import indic_transliteration.sanscript as sanscript
+import re
 
-
-def main():
+def replaceText():
     repl_list = [# ('र्द्ध्', 'र्ध्'), ('र्द्ध', 'र्ध'),
                  ('त्स्', 'थ्स्'), ('प्स्', 'फ्स्'),
                  ('त्स', 'थ्स'), ('प्स', 'फ्स'),
@@ -26,6 +27,22 @@ def main():
         if not noReplaces:
             with open(file, 'w') as out_f:
                 out_f.writelines(file_lines)
+
+
+def main():
+    for file in sys.argv[1:]:
+        # sys.stderr.write('Updating %s\n' % (file))
+        with open(file, 'r') as in_f:
+            file_lines = in_f.readlines()
+
+            for i, line in enumerate(file_lines):
+                for word in line.split(' '):
+                    trans_word = sanscript.transliterate(word, 'devanagari', 'itrans')
+                    if trans_word == word:
+                        continue
+                    trans_word_rep = re.sub('([kgcjtdpb])s', '\\1hs', trans_word)
+                    if trans_word_rep != trans_word:
+                        print('%40s:%05d:: %s' % (file, i, trans_word))
 
 
 if __name__ == '__main__':
