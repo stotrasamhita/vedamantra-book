3fb0c2532fe0a9624071cd510fdb9b65c76bad76
Karthik Raman <karthikraman@users.noreply.github.com>
2020-12-22 16:56:32 +0530

    Adding separate files

---
+++ kandas/splitPrashnas.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+mkdir -p .old/
+mv TaittiriyaSamhita-Kandam-*.tex TaittiriyaSamhita-[1234567]*.tex .old/
+awk 'BEGIN{c=0; p=0}
+/chapt/{c++;p=0}
+/sect/{p++;}
+/chapt/,/%%% END KANDAM/{print > "TaittiriyaSamhita-Kandam-" c ".tex"}
+/sect/,/%%% END PRASHNA/{print > "TaittiriyaSamhita-" c "-" p ".tex"}' TaittiriyaSamhita-Kandas.tex
+rm TaittiriyaSamhita-*0.tex
+cd .old/
+for x in *.tex; do diff $x ../$x -q && mv $x ../; done
+#for x in *.tex; do diff $x ../$x -qs && mv -v $x ../; done
+cd ..
