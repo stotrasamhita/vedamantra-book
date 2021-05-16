b873ad7de3a83c14f7b725c889ca1db8f2e4b5ed
Karthik Raman <karthikraman@users.noreply.github.com>
2021-05-16 09:13:35 +0530

    Update splitPrashnas.sh

--- kandas/splitPrashnas.sh
+++ kandas/splitPrashnas.sh
@@ -7,6 +7,7 @@ awk 'BEGIN{c=0; p=0}
 /chapt/,/%%% END KANDAM/{print > "TaittiriyaSamhita-Kandam-" c ".tex"}
 /sect/,/%%% END PRASHNA/{print > "TaittiriyaSamhita-" c "-" p ".tex"}' TaittiriyaSamhita-Kandas.tex
 # rm TaittiriyaSamhita-*0.tex
+cp  TaittiriyaSamhita-Kandas*.tex .old/
 cd .old/
 for x in *.tex; do diff $x ../$x -q && mv $x ../; done
 #for x in *.tex; do diff $x ../$x -qs && mv -v $x ../; done
