d9a387032759c31edca21c537c2c74408ee8eb18
Karthik Raman <karthikraman@users.noreply.github.com>
2022-02-17 22:04:31 +0530

    Update performSiddhantaFontFixes.py

--- performSiddhantaFontFixes.py
+++ performSiddhantaFontFixes.py
@@ -3,7 +3,7 @@ import sys
 import indic_transliteration.sanscript as sanscript
 import re
 
-repl_list = [('ष्ट्य', 'ट्य')]
+repl_list = [('ष्ट्य', 'ट्य'), ('ष्ठ्य', 'ठ्य')]
 for file in sys.argv[1:]:
     sys.stderr.write('Updating %s\n' % (file))
     with open(file, 'r') as in_f:
@@ -11,6 +11,7 @@ for file in sys.argv[1:]:
 
     noReplaces = True
     for old, new in repl_list:
+        print(old, '->', new)
         for i in range(len(file_lines)):
             file_lines_new = file_lines[i].replace(old, new)
             if file_lines_new != file_lines[i]:
