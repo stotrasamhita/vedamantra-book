1b93fa257beb3851e4416c21007b1186d5881987
Karthik Raman <karthikraman@users.noreply.github.com>
2023-11-18 23:09:07 +0530

    PDFs updated

--- aranyakas-kindle-pdf/tex2oldpdf.sh
+++ aranyakas-kindle-pdf/tex2oldpdf.sh
@@ -7,7 +7,7 @@ echo "File path         : $fpath"
 jobname=`echo $fpath | sed 's/.tex//;s@.*aranyakas/@@'`
 echo "PDF target        : $jobname.pdf"
 mkdir -p `dirname $jobname`
-if [[ $fpath -nt $jobname.pdf || aranyaka-oldkindle-template.tex -nt $jobname-old.pdf ]]
+if [[ $fpath -nt $jobname-old.pdf || aranyaka-oldkindle-template.tex -nt $jobname-old.pdf ]]
 then
 echo Rebuilding $jobname-old.pdf... > /dev/stderr
 echo Rebuilding $jobname-old.pdf...
